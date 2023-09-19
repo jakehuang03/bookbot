a96d74b7c1877a0100413f9b23be7a190a5774e4
jakehuang03 <zijianhuang03@gmail.com>
2023-09-18 22:19:54 -0400

    inital react setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import api from './api'
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                Hello World
+            </h1>
+        </div>
+    )
+}
+
+export default App;
