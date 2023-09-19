17574fe25386a7fb2458b2885c4fb654e0411a50
YingrongChen <yingrong.chen@emory.edu>
2023-09-19 15:35:10 -0400

    reset last unreviewed commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import api from './api'
-import FileUpload from './FileUpload';
 
 const App = () => {
     return (
         <div>
             <h1>
                 Hello World
-                <FileUpload />
             </h1>
         </div>
     )
