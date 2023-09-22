a2c55a2e24df712b92c87e68432021ef178b46a4
jakehuang03 <zijianhuang03@gmail.com>
2023-09-22 16:43:05 -0400

    remove uncessary

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -65,72 +65,10 @@ const Navbar = () => {
         signInButton = <Button component={Link} to="/auth" variant="contained" style={{ background: '#FFCD3B', color: 'black' }}>Sign In</Button>
     }
 
-  if (user?.result) {
-    signInButton = (
-      <Box sx={{ flexGrow: 0 }}>
-        <Avatar alt={user?.result.name} src={user?.result.imageUrl}>
-          {user?.result.name.charAt(0)}
-        </Avatar>
-        <Button variant="contained" color="secondary" onClick={logout}>
-          Logout
-        </Button>
-      </Box>
-    );
-  } else if (user?.sub) {
-    signInButton = (
-      <Box sx={{ flexGrow: 0 }}>
-        <Avatar alt={user?.name} src={user?.picture}>
-          {user?.name.charAt(0)}
-        </Avatar>
-        <Typography variant="h6">{user?.name}</Typography>
-        <Button variant="contained" color="secondary" onClick={logout}>
-          Logout
-        </Button>
-      </Box>
-    );
-  } else {
-    signInButton = (
-      <Button
-        component={Link}
-        to="/auth"
-        variant="contained"
-        style={{ background: "#E3D026" }}
-      >
-        Sign In
-      </Button>
-    );
-  }
-
   return (
     <AppBar position="static" style={{ background: "#2E3B55" }}>
       <Container>
         <Toolbar>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {/* {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))} */}
-            </Menu>
-          </Box>
           <Link to="/">
             <img src={logo} alt="icon" height="40px" />
           </Link>
