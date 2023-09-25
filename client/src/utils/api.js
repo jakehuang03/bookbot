ea909f4e518ad1bd272e4a5d3eb5d22db9125837
jakehuang03 <zijianhuang03@gmail.com>
2023-09-24 21:12:12 -0400

    update api baseURL and functions

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,13 +2,21 @@ import axios from 'axios';
 import store from '../store';
 import {LOGOUT} from '../actions/types'
 
-// Create an instance of axios
-const api = axios.create({
-  baseURL: '/api',
-  headers: {
-    'Content-Type': 'application/json'
+const api = axios.create(
+  { baseURL: 'https://localhost:8000' }
+);
+
+api.interceptors.request.use((req) => {
+  if (localStorage.getItem('profile')) {
+      if(JSON.parse(localStorage.getItem('profile'))?.token)
+          req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+      else 
+          req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).sub}`;
   }
+
+  return req;
 });
+
 /*
   NOTE: intercept any error responses from the api
  and check if the token is no longer valid.
@@ -26,5 +34,10 @@ api.interceptors.response.use(
     return Promise.reject(err);
   }
 );
+
+export const loadUser = (formData) => api.post('/api/user/loadUser', formData);
+export const register = (formData) => api.post('/api/user/register', formData);
+export const login = (email, password) => api.post('/api/user/lgoin', email, password);
+
 
 export default api;
