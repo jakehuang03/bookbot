14c2ffff68c6b72e3625bcd2df6a5a98e575bc6a
Lupin Cai <lupin.cai@emory.edu>
2023-09-27 19:51:14 -0400

    feature: keyword extraction done

---
+++ backend/chatbot/api/KeyWord.py
@@ -0,0 +1,30 @@
+import nltk
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize
+
+# Ensure resources are downloaded
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
+
+import spacy
+class extract:
+    def __init__(self, text):
+        self.words = word_tokenize(text)
+
+    def extract_verbs(self):
+        pos_tags = pos_tag(self.words)
+        verbs = [word for word, tag in pos_tags if tag.startswith('VB')]
+        return verbs
+    def extract_nouns(self):
+        pos_tags = pos_tag(self.words)
+        nouns = [word for word, tag in pos_tags if tag.startswith('NN') or tag.startswith('NNS')]
+        return nouns
+
+
+if __name__ == "__main__":
+
+    test = extract("what is scrum?")
+    verbs = test.extract_verbs()
+    noun = test.extract_nouns()
+    print(verbs)
+    print(noun)
