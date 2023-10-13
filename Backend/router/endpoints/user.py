67c886acf308e7e129eec43c96cc05b84754ec9f
zipeng <zipeng_ye@outlook.com>
2023-10-13 00:49:05 -0400

    feature: JWT token

--- Backend/router/endpoints/user.py
+++ Backend/router/endpoints/user.py
@@ -1,12 +1,27 @@
 import db.crud
-from utils.user import hash_password, decode_token, verify_password
-from typing import Annotated
+from datetime import datetime, timedelta
+from utils.user import hash_password, verify_password
+from typing import Annotated, Union
 from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user/token")
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 @router.post("/token")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
@@ -17,7 +32,12 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
 
     if not verify_password(form_data.password,user["UserPassword"]):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
-    return {"access_token": user["UserEmail"], "token_type": "bearer"}
+    
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user["UserEmail"]}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @router.post("/books")
@@ -51,13 +71,19 @@ async def signup(
     return {"msg":"signup successed"}
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = db.crud.get_user_by_email(username)
     user = user.__dict__
     return user
 
