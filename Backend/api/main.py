6f49cb5a207e00944dcd070c93c93be969200ba5
Lupin Cai <lupin.cai@emory.edu>
2023-10-12 21:39:03 -0400

    feature: refactored

--- Backend/api/main.py
+++ Backend/api/main.py
@@ -1,31 +1,5 @@
-from ToLLM.localLLMCall import localcall
-from preLLM.KeyWordHuggingFace import extract
-from preLLM.WordSearch import WordSearch
+from testFunction import callwithquestions
 
 if __name__ == "__main__":
-    question = "give me the number of scenes that helen was talking with the voice"
-    question2 = ["what do directors do?","why do we need directors"]
-
-
-    # initialize tensor and word finding
-    bookname = "b.pdf"
-    word2 = extract(question2[0])
-    print(word2[0])
-    word = word2[0]
-    word_search = WordSearch(bookname)
-    word_positions = word_search.find_word(word)
-    context = word_search.sentences_around_index(word_positions, 2)
-
-    # wording finding
-    for pos, surrounding_sentences in context.items():
-        page_num = word_search.position_to_page_number(pos)
-        print(f"At position {pos} (Page {page_num + 1}):")
-        for sentence in surrounding_sentences:
-            print(sentence)
-        print("\n")
-
-
-
-    # asking the llm
-    response = localcall(context, question2)
-    print(response)
+    question = ["what do the director do?"]
+    callwithquestions("b.pdf", question)
---
+++ Backend/api/testFunction.py
@@ -0,0 +1,25 @@
+from ToLLM.localLLMCall import localcall
+from preLLM.KeyWordHuggingFace import extract
+from preLLM.WordSearch import WordSearch
+
+def callwithquestions(file, question):
+    # initialize tensor and word finding
+    bookname = file
+    word2 = extract(question[0])
+    print(word2[0])
+    word = word2[0]
+    word_search = WordSearch(bookname)
+    word_positions = word_search.find_word(word)
+    context = word_search.sentences_around_index(word_positions, 2)
+
+    # wording finding
+    for pos, surrounding_sentences in context.items():
+        page_num = word_search.position_to_page_number(pos)
+        print(f"At position {pos} (Page {page_num + 1}):")
+        for sentence in surrounding_sentences:
+            print(sentence)
+        print("\n")
+
+    # asking the llm
+    response = localcall(context, question)
+    print(response)
