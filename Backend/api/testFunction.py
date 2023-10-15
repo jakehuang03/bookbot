8d032a388e67ec3c98ade0b6699df00ea5696af5
Lupin Cai <lupin.cai@emory.edu>
2023-10-15 16:53:10 -0400

    feature: cnnted, before change to return a map

--- Backend/api/testFunction.py
+++ Backend/api/testFunction.py
@@ -1,3 +1,4 @@
+import os
 from ToLLM.localLLMCall import localcall
 from preLLM.KeyWordHuggingFace import extract
 from preLLM.WordSearch import WordSearch
@@ -5,7 +6,7 @@ from preLLM.WordSearch import WordSearch
 def callwithquestions(file, question):
     # initialize tensor and word finding
     bookname = file
-    word2 = extract(question[0])
+    word2 = extract(question)
     print(word2[0])
     word = word2[0]
     word_search = WordSearch(bookname)
@@ -24,7 +25,22 @@ def callwithquestions(file, question):
     response = localcall(context, question)
     print(response)
 
+def ask_question(book, question):
+
+        # Access the "book" and "questions" fields from the request_data dictionary
+        s = book
+        print(book)
+
+        current_directory = os.path.dirname(__file__)
+        folder_name = "upload_files"  # Replace with the name of your folder
+        folder_path = os.path.join(current_directory, folder_name).replace('\\', '\\\\')
+
+        path = os.path.join(folder_path, s)
+        print(path)
+
+        result = callwithquestions(path, question)
+        return result
 
 if __name__ == "__main__":
-    question = ["what do the director do?"]
-    callwithquestions("b.pdf", question)
+    question = "what do the director do?"
+    ask_question("b.pdf", question)
