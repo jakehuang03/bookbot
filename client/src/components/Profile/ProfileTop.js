8e47ce61db6397ea017f83b14fbf4b045b65578e
DaisyWang <zijun.wang@emory.edu>
2023-10-16 15:14:37 -0400

    fix avatar on profile

--- client/src/components/Profile/ProfileTop.js
+++ client/src/components/Profile/ProfileTop.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const avatar = require("./avatar.png");
+
 const ProfileTop = ({}) => {
 	return (
 		<div className='profile-top bg-primary p-2'>
-			<img className='round-img my-1' src='./avatar.img' alt='' />
+			<img className='round-img my-1' src={avatar} alt='a' />
 			<h1 className='large'>Daisy Wang</h1>
 			<p className='lead'>CS Student @ Emory University</p>
 			<p>Atlanta, GA</p>
