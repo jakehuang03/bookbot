d222d2397aed61bbe907d51b1a7ad579a416f61e
DaisyWang <zijun.wang@emory.edu>
2023-10-16 14:57:05 -0400

    Proto of profile page

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,94 +1,91 @@
-import React, { Fragment, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { connect, useDispatch } from 'react-redux';
-import PropTypes from 'prop-types';
-import { login } from '../../actions/auth';
-import { GoogleLogin } from '@react-oauth/google';
+import React, { Fragment, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { connect, useDispatch } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
+import { GoogleLogin } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
 
-const Login = ({login}) => {
-  const [formData, setFormData] = useState({
-    username: '',
-    password: ''
-  });
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+const Login = ({ login }) => {
+	const [formData, setFormData] = useState({
+		username: "",
+		password: "",
+	});
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-  const { username, password } = formData;
+	const { username, password } = formData;
 
-  const onChange = (e) =>
-    setFormData({ ...formData, [e.target.name]: e.target.value }); //[e.target.name] create a dynamic key, e.target.value is assigned to the key
+	const onChange = (e) =>
+		setFormData({ ...formData, [e.target.name]: e.target.value }); //[e.target.name] create a dynamic key, e.target.value is assigned to the key
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    login(username, password, navigate);
-  };
+	const onSubmit = (e) => {
+		e.preventDefault();
+		login(username, password, navigate);
+	};
 
-  const handleCallBack = (response) => {
-    var userObject = jwt_decode(response.credential);
-    console.log(userObject);
-        try {
-            dispatch({type: "AUTH", data: userObject});
-            navigate('/');
-        } catch (error) {
-            console.log(error);
-        }
-  };
-  
-  const errorMessage = (error) => {
-      console.log(error);
-  };
+	const handleCallBack = (response) => {
+		var userObject = jwt_decode(response.credential);
+		console.log(userObject);
+		try {
+			dispatch({ type: "AUTH", data: userObject });
+			navigate("/");
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <Fragment>
-      <div className="loginsignin">
-        <h1 className="large text-primary">Sign In</h1>
-        <p className="lead">
-          Sign Into Your Account
-        </p>
-        <form className="form" onSubmit={onSubmit}>
-          <div className="form-group">
-            <input
-              type="email"
-              placeholder="Email"
-              name="username"
-              value={username}
-              onChange={onChange}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              value={password}
-              onChange={onChange}
-              minLength="6"
-              required
-            />
-          </div>
-          <input type="submit" className="btn btn-primary" value="Login" />
-        </form>
-        <div className="googleLog">
-        <GoogleLogin onSuccess={handleCallBack} onError={errorMessage} />
-        </div>
-        <p className="my-1">
-          Don't have an account? <Link to="/register">Sign Up</Link>
-        </p>
-      </div>
-      
-    </Fragment>
-  );
+	const errorMessage = (error) => {
+		console.log(error);
+	};
+
+	return (
+		<Fragment>
+			<div className='contentBox'>
+				<h1 className='large text-primary'>Sign In</h1>
+				<p className='lead'>Sign Into Your Account</p>
+				<form className='form' onSubmit={onSubmit}>
+					<div className='form-group'>
+						<input
+							type='email'
+							placeholder='Email'
+							name='username'
+							value={username}
+							onChange={onChange}
+							required
+						/>
+					</div>
+					<div className='form-group'>
+						<input
+							type='password'
+							placeholder='Password'
+							name='password'
+							value={password}
+							onChange={onChange}
+							minLength='6'
+							required
+						/>
+					</div>
+					<input type='submit' className='btn btn-primary' value='Login' />
+				</form>
+				<div className='googleLog'>
+					<GoogleLogin onSuccess={handleCallBack} onError={errorMessage} />
+				</div>
+				<p className='my-1'>
+					Don't have an account? <Link to='/register'>Sign Up</Link>
+				</p>
+			</div>
+		</Fragment>
+	);
 };
 
 Login.propTypes = {
-  login: PropTypes.func.isRequired,
-  // isAuthenticated: PropTypes.bool
+	login: PropTypes.func.isRequired,
+	// isAuthenticated: PropTypes.bool
 };
 
 const mapStateToProps = (state) => ({
-  // isAuthenticated: state.auth.isAuthenticated
+	// isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(mapStateToProps, { login })(Login);
---
+++ client/src/components/Profile/ProfileTop.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ProfileTop = ({}) => {
+	return (
+		<div className='profile-top bg-primary p-2'>
+			<img className='round-img my-1' src='./avatar.img' alt='' />
+			<h1 className='large'>Daisy Wang</h1>
+			<p className='lead'>CS Student @ Emory University</p>
+			<p>Atlanta, GA</p>
+			<div className='icons my-1'>
+				<a href='#' target='_blank' rel='noopener noreferrer'>
+					<i className='fas fa-globe fa-2x'></i>
+				</a>
+
+				<a href='#' target='_blank' rel='noopener noreferrer'>
+					<i className='fab fa-twitter fa-2x'></i>
+				</a>
+
+				<a href='#' target='_blank' rel='noopener noreferrer'>
+					<i className='fab fa-facebook fa-2x'></i>
+				</a>
+
+				<a href='#' target='_blank' rel='noopener noreferrer'>
+					<i className='fab fa-linkedin fa-2x'></i>
+				</a>
+
+				<a href='#' target='_blank' rel='noopener noreferrer'>
+					<i className='fab fa-youtube fa-2x'></i>
+				</a>
+
+				<a href='#' target='_blank' rel='noopener noreferrer'>
+					<i className='fab fa-instagram fa-2x'></i>
+				</a>
+			</div>
+		</div>
+	);
+};
+
+ProfileTop.propTypes = {};
+
+export default ProfileTop;
