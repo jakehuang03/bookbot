3246330e051467c57057d87acae45e7a8737f778
Lupin Cai <lupin.cai@emory.edu>
2023-10-22 18:53:12 -0400

    feature: refactoring

--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,16 @@ from Backend.db.crud import get_book_by_name
 from router.api import api_router
 from fastapi.middleware.cors import CORSMiddleware
 
+import shutil
+from pathlib import Path
+from fastapi import FastAPI, HTTPException, Request, UploadFile,File
+from fastapi.middleware.cors import CORSMiddleware
+from Backend.api.preLLM.refactorexp import store_tensor_in_db
+from Backend.db.database import SessionLocal
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+import PyPDF2
+
 app = FastAPI()
 origins = ["*"]
 app.add_middleware(
@@ -43,5 +53,44 @@ async def searchBar(request: Request):
         genre = request.query_params.get("tag")
 
         return get_book_by_name(bookname, genre)
+    except Exception as e:
+        raise HTTPException(detail=f"An error occurred: {e}", status_code=400)
+    
+    
+@app.post("/books")
+async def upload_file(file: UploadFile = File(...)):
+    try:
+        # Save the uploaded file locally
+        upload_folder = Path("api/uploaded_files")
+        upload_folder.mkdir(exist_ok=True)
+
+        filepath = upload_folder / file.filename
+        with filepath.open("wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        # Convert PDF to string
+        with filepath.open("rb") as pdf_file:
+            reader = PyPDF2.PdfReader(pdf_file)
+            pages_text = [reader.pages[page_num].extract_text() for page_num in range(len(reader.pages))]
+        
+        book_as_continuous_text = ''.join(pages_text)
+
+        # Convert the string to a tensor
+        maxvocab = len(set(book_as_continuous_text.split()))
+        tokenizer = Tokenizer(num_words=maxvocab, oov_token='<OOV>')
+        tokenizer.fit_on_texts([book_as_continuous_text])
+        sequences = tokenizer.texts_to_sequences([book_as_continuous_text.split()])[0]
+        tensor = tf.convert_to_tensor(sequences, dtype=tf.int32)
+
+        # Store tensor and tokenizer to the database
+        session = SessionLocal()
+        store_tensor_in_db(file.filename, tensor, tokenizer, session)
+        session.close()
+
+        return {
+            "filename": file.filename,
+            "content_type": file.content_type
+        }
+
     except Exception as e:
         raise HTTPException(detail=f"An error occurred: {e}", status_code=400)
