3f4c0e125ebfb376c3d9624932d0cc7c80dca99c
jakehuang03 <zijianhuang03@gmail.com>
2023-10-26 19:09:10 -0400

    create upload and receive for s3

--- Backend/router/endpoints/s3.py
+++ Backend/router/endpoints/s3.py
@@ -1,12 +1,15 @@
 # pip3 install boto3
 import boto3 as aws
-
+import logging
+from botocore.exceptions import ClientError
 
 s3 = aws.client(
     service_name="s3",
     region_name="us-east-2",
-    aws_access_key_id="AKIARXGGAUFZQL53JF46",
-    aws_secret_access_key="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",
+    # aws_access_key_id="AKIARXGGAUFZQL53JF46",
+    # aws_secret_access_key="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"
+    aws_access_key_id="AKIARXGGAUFZX5Q2SH27",
+    aws_secret_access_key="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",
 )
 
 
@@ -20,3 +23,25 @@ def generateUploadURL():
         ExpiresIn=60,
     )
     return url
+
+
+def s3_upload(file, destination, userId):
+    try:
+        s3.upload_file('Backend/router/endpoints/user image.jpg', 'bookbotimg', 'user_image/user image.jpg')
+        # s3.upload_file(file, 'bookbotimg', 'user_image/'+userId+)
+    except ClientError as e:
+        logging.error(e)
+
+def s3_retrieve(destination):
+    try:
+        obj = s3.get_object(Bucket='bookbotimg', Key=destination)
+    except ClientError as e:
+        logging.error(e)
+    else:
+        return obj
+
+
+# response = s3_retrieve('user_image/user image.jpg')
+# print(response['Body'].read())
+
+# s3_upload()
