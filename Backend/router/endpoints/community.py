4dffb4796621ea9f391826790fcab86467761472
zipeng <zipeng_ye@outlook.com>
2023-10-29 00:07:47 -0400

    feature: getques

--- Backend/router/endpoints/community.py
+++ Backend/router/endpoints/community.py
@@ -1,4 +1,4 @@
-import db
+import db.crud as crud
 from fastapi import APIRouter, HTTPException, UploadFile, File, Form
 
 router = APIRouter()
@@ -11,10 +11,21 @@ async def save_question(
     answer: str = Form(...)
 ):
     try:
-        id = db.crud.create_question(
+        id = crud.create_question(
             userid=userid, bookid=bookid, content=content, answer=answer
         )
         return {"msg": "question saved", "questionid": id}
     except Exception as e:
         raise HTTPException(detail=f"An error occurred: {e}", status_code=400)
     
+@router.get("/getquesbyuser/{userid}")
+async def get_ques_by_user(userid):
+    return crud.get_question_by_userid(userid=userid)
+
+@router.get("/getquesbybook/{bookid}")
+async def get_ques_by_book(bookid):
+    return crud.get_question_by_bookid(bookid=bookid)
+
+@router.get("/getques")
+async def get_ques_all():
+    return crud.get_question_all()
