056e7edcf4ea6d079732128afb76173c813a5f5d
Lupin Cai <lupin.cai@emory.edu>
2023-11-05 18:41:53 -0500

    feature: search

--- Backend/db/crud.py
+++ Backend/db/crud.py
@@ -31,6 +31,8 @@ def create_user_profile(userid: int, name: str, bio: str, avatar: str, gender: s
 
 
 def create_book(name: str, author: str, summary: str, userid: str, genre: str):
+    if genre is None:
+        genre = "none"
     db_book = database.Book(
         BookName=name, Author=author, BookContent=summary, UserId=userid, Genre=genre
     )
@@ -67,39 +69,38 @@ def get_book_by_name(bookname: str, genre: str):
         return query1.all()
 
 def get_mybook_by_name(bookname: str, genre: str, userId: int):
-    userId = str(userId)
     if bookname == "none" and genre == "none" and userId == "none":
         query = db.query(database.Book).all()
         return query
     # single none
-    elif genre == "none":
+    elif genre == "none" and bookname != "none" and userId != "none":
         query = db.query(database.Book).filter(
             database.Book.BookName.like("%" + bookname + "%"),
             database.Book.UserId == userId
         )
         return query.all()
-    elif bookname == "none":
+    elif bookname == "none" and genre != "none" and userId != "none":
         query = db.query(database.Book).filter(
             database.Book.Genre == genre,
             database.Book.UserId == userId)
         return query.all()
-    elif userId == "none":
+    elif userId == "none" and bookname != "none" and genre != "none":
         query = db.query(database.Book).filter(
             database.Book.BookName.like("%" + bookname + "%"),
             database.Book.Genre == genre
         )
         return query.all()
     # double none
-    elif genre == "none" and bookname == "none":
+    elif genre == "none" and bookname == "none" and userId != "none":
         query = db.query(database.Book).filter(
             database.Book.UserId == userId)
         return query.all()
-    elif genre == "none" and userId == "none":
+    elif genre == "none" and userId == "none" and bookname != "none":
         query = db.query(database.Book).filter(
             database.Book.BookName.like("%" + bookname + "%")
         )
         return query.all()
-    elif userId == "none" and bookname == "none":
+    elif userId == "none" and bookname == "none" and genre != "none":
         query = db.query(database.Book).filter(
             database.Book.Genre == genre
         )
