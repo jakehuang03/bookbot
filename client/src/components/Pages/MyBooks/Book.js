cbc618a4c0edbb04600ac8dc702d1296ae01b378
jakehuang03 <zijianhuang03@gmail.com>
2023-11-06 16:21:30 -0500

    add published icon for mybooks

---
+++ client/src/components/Pages/MyBooks/Book.js
@@ -0,0 +1,78 @@
+import * as React from "react";
+// import PropTypes from "prop-types";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { Link as RouterLink } from "react-router-dom";
+import defaultCover from '../../../images/Default Book Cover.jpg';
+
+function Book(props) {
+  const { book } = props;
+  const image = book?.image ? book.image : defaultCover;
+
+  const handleImageError = (e) => {
+    e.target.onerror = null; 
+    e.target.src = defaultCover;
+  }
+  
+  return (
+    <Grid item xs={6} md={3}>
+      <CardActionArea component={RouterLink} to={`/books/${book.BookId}`}>
+        <Card>
+          <CardMedia
+            component="img"
+            alt={book.BookName}
+            height='160'
+            image={image}
+            onError={handleImageError}
+          />
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {book.BookName}
+            </Typography>
+            {/* <Typography variant="h6" color="text.secondary">
+              {book.Author}
+            </Typography> */}
+            {/* <Typography variant="h6" noWrap>
+              {book.BookContent}
+            </Typography> */}
+            <div
+              style={{
+                position: "absolute",
+                top: "0",
+                right: "0",
+                backgroundColor: "white",
+                padding: "5px",
+                borderRadius: "5px",
+                display: "inline-block",
+              }}
+            >
+              <Typography
+                variant="body2"
+                color="textSecondary"
+                style={{ color: "darkblue", fontWeight: "bold" }}
+              >
+                {book.isPublished ? "Published" : "Unpublished"}
+              </Typography>
+            </div>
+          </CardContent>
+        </Card>
+      </CardActionArea>
+    </Grid>
+  );
+}
+
+// Book.propTypes = {
+//   book: PropTypes.shape({
+//     BookId: PropTypes.number.isRequired,
+//     BookName: PropTypes.string.isRequired,
+//     Author: PropTypes.string.isRequired,
+//     BookContent: PropTypes.string.isRequired,
+//     // image: PropTypes.string.isRequired,
+//   }).isRequired,
+// };
+
+export default Book;
--- client/src/components/Pages/MyBooks/MyBooks.js
+++ client/src/components/Pages/MyBooks/MyBooks.js
@@ -2,7 +2,7 @@ import React, {useEffect} from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import Book from "../SharedComponents/Book";
+import Book from "./Book";
 import SearchGenre from "./Search_Genre";
 import { useSelector, useDispatch } from "react-redux";
 import {getMyBooks} from "../../../actions/books"
