1f1ca5598d8ac14ffdd02990d4e2bbf368e0e847
DaisyWang <zijun.wang@emory.edu>
2023-11-06 17:50:46 -0500

    more margin on community page

--- client/src/components/Pages/Community/Community.js
+++ client/src/components/Pages/Community/Community.js
@@ -14,26 +14,33 @@ import Post from "./Post";
  */
 
 const Community = () => {
-  const dispatch = useDispatch();
-  dispatch(getQuestion());
-  const PostList = JSON.parse(sessionStorage.getItem("post_list"));
-  const [sources, setSources] = useState([]);
-  return (
-    <Container>
-    <Typography className='bg-primary' variant="h1" align="center" sx={{ p: 4, mb:4 }}>
-        Community
-    </Typography>
+	const dispatch = useDispatch();
+	dispatch(getQuestion());
+	const PostList = JSON.parse(sessionStorage.getItem("post_list"));
+	const [sources, setSources] = useState([]);
+	return (
+		<Container className='contentBox'>
+			<Typography
+				className='bg-primary'
+				variant='h1'
+				align='center'
+				sx={{ p: 4, mb: 4 }}
+			>
+				Community
+			</Typography>
 
-    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-      {Array.isArray(sources) 
-      ? sources.map((post) => (
-        <Post key={post.QuestionId} post={post} />
-      ))
-      : []}
-    </Grid>
-    <SourcePagination setSources={setSources} fullSources={PostList} pageSize='5' />
-    </Container>
-  );
+			<Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+				{Array.isArray(sources)
+					? sources.map((post) => <Post key={post.QuestionId} post={post} />)
+					: []}
+			</Grid>
+			<SourcePagination
+				setSources={setSources}
+				fullSources={PostList}
+				pageSize='5'
+			/>
+		</Container>
+	);
 };
 
 export default Community;
