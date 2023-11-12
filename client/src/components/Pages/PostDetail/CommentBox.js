8252193a4e7de79517ec9bf7510647f918c7e29d
DaisyWang <zijun.wang@emory.edu>
2023-11-11 20:59:17 -0500

    Connect comment input box avatar with auth avatar

--- client/src/components/Pages/PostDetail/CommentBox.js
+++ client/src/components/Pages/PostDetail/CommentBox.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { IconButton, InputBase } from "@mui/material";
 import Paper from "@mui/material/Paper";
@@ -14,77 +14,82 @@ import { saveComment } from "../../../actions/community";
  * @returns {JSX.Element} The CommentBox component.
  */
 const CommentBox = () => {
-  const user = JSON.parse(localStorage.getItem("profile"));
+	const { avatar } = useSelector((state) => state.auth);
+	const user = JSON.parse(localStorage.getItem("profile"));
 
-  const [commentValue, setCommentValue] = useState("");
+	const [commentValue, setCommentValue] = useState("");
 
-  const [rows, setRows] = useState("1");
-  const dispatch = useDispatch();
+	const [rows, setRows] = useState("1");
+	const dispatch = useDispatch();
 
-  const onChange = (e) => {
-    setCommentValue(e.target.value);
-  };
+	const onChange = (e) => {
+		setCommentValue(e.target.value);
+	};
 
-  /**
-   * Resets the comment input field and rows state.
-   */
-  const onClose = () => {
-    setCommentValue("");
-    setRows("1");
-  };
+	/**
+	 * Resets the comment input field and rows state.
+	 */
+	const onClose = () => {
+		setCommentValue("");
+		setRows("1");
+	};
 
-  /**
-   * Dispatches the comment to be saved and resets the comment input field and rows state.
-   * @param {Object} e - The event object.
-   */
-  const onSubmit = (e) => {
-    dispatch(saveComment(commentValue));
-    onClose();
-  };
+	/**
+	 * Dispatches the comment to be saved and resets the comment input field and rows state.
+	 * @param {Object} e - The event object.
+	 */
+	const onSubmit = (e) => {
+		dispatch(saveComment(commentValue));
+		onClose();
+	};
 
-  const handleKeyPress = (event) => {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      onSubmit();
-    }
-  };
+	const handleKeyPress = (event) => {
+		if (event.keyCode === 13) {
+			event.preventDefault();
+			onSubmit();
+		}
+	};
 
-  const onClick = () => {
-    setRows("4");
-  };
-  return (
-    <Paper
-      elevation={8}
-      component="form"
-      sx={{ display: "flex", alignItems: "center", m: 2 }}
-    >
-      <Avatar src={user?.picture} sx={{ m: 1 }}>
-        H
-      </Avatar>
-      <InputBase
-        placeholder="Add a comment"
-        sx={{ flex: 1, m: 2 }}
-        onKeyDown={handleKeyPress}
-        onChange={onChange}
-        onClick={onClick}
-        multiline
-        rows={rows}
-        value={commentValue}
-        inputProps={{ style: { fontSize: 20 } }}
-      />
+	const onClick = () => {
+		setRows("4");
+	};
+	return (
+		<Paper
+			elevation={8}
+			component='form'
+			sx={{ display: "flex", alignItems: "center", m: 2 }}
+		>
+			<Avatar
+				src={`data:image/jpeg;base64,${avatar}`}
+				alt={user?.name}
+				sx={{ width: 35, height: 35, marginLeft: 1 }}
+			>
+				{user?.name.charAt(0)}
+			</Avatar>
+			<InputBase
+				placeholder='Add a comment'
+				sx={{ flex: 1, m: 2 }}
+				onKeyDown={handleKeyPress}
+				onChange={onChange}
+				onClick={onClick}
+				multiline
+				rows={rows}
+				value={commentValue}
+				inputProps={{ style: { fontSize: 20 } }}
+			/>
 
-      <IconButton
-        type="button"
-        onClick={onSubmit}
-        disabled={commentValue?.length < 1}
-      >
-        <ReplyIcon />
-      </IconButton>
-      <IconButton onClick={onClose}>
-        <ClearIcon />
-      </IconButton>
-    </Paper>
-  );
+			<IconButton
+				type='button'
+				onClick={onSubmit}
+				disabled={commentValue?.length < 1}
+			>
+				<ReplyIcon />
+			</IconButton>
+			<IconButton onClick={onClose}>
+				<ClearIcon />
+			</IconButton>
+		</Paper>
+	);
 };
 
 export default CommentBox;
