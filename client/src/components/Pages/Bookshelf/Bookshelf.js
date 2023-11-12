e4f0bffa74984e6ca0693c96c70361c77e8ee267
DaisyWang <zijun.wang@emory.edu>
2023-11-11 20:50:55 -0500

    Restyle titles

--- client/src/components/Pages/Bookshelf/Bookshelf.js
+++ client/src/components/Pages/Bookshelf/Bookshelf.js
@@ -1,42 +1,42 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Book from "../SharedComponents/Book";
 import SearchGenre from "./Search_Genre";
 import { useSelector, useDispatch } from "react-redux";
-import {getBooks} from "../../../actions/books"
+import { getBooks } from "../../../actions/books";
 
 export default function BookShelf() {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getBooks());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(getBooks());
+	}, [dispatch]);
 
-  const {books} = useSelector((state) => state.books);
+	const { books } = useSelector((state) => state.books);
 
-  return (
-    <>
-      <Container>
-        <Typography variant="h2" align="center" marginTop='30px'>
-          Bookshelf
-        </Typography>
-      </Container>
-      <div>
-        <Grid container spacing={2} justifyContent="center">
-          <Grid item>
-            <SearchGenre />
-          </Grid>
-        </Grid>
-      </div>
-      <Container sx={{ py: 8 }} maxWidth="lg">
-        <Grid container spacing={4}>
-          {books.map((book) => (
-            <Book key={book.BookId} book={book} />
-          ))}
-        </Grid>
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<Container>
+				<div className='header'>
+					<h1 className='header-text'>Bookshelf</h1>
+				</div>
+			</Container>
+			<div>
+				<Grid container spacing={2} justifyContent='center'>
+					<Grid item>
+						<SearchGenre />
+					</Grid>
+				</Grid>
+			</div>
+			<Container sx={{ py: 8 }} maxWidth='lg'>
+				<Grid container spacing={4}>
+					{books.map((book) => (
+						<Book key={book.BookId} book={book} />
+					))}
+				</Grid>
+			</Container>
+		</>
+	);
 }
--- client/src/components/Pages/MyBooks/MyBooks.js
+++ client/src/components/Pages/MyBooks/MyBooks.js
@@ -1,47 +1,47 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Book from "./Book";
 import SearchGenre from "./Search_Genre";
 import { useSelector, useDispatch } from "react-redux";
-import {getMyBooks} from "../../../actions/books"
+import { getMyBooks } from "../../../actions/books";
 import UploadBookButton from "./UploadBookButton";
 
 export default function MyBooks() {
-    const dispatch = useDispatch();
-    const user = JSON.parse(localStorage.getItem('profile'));
+	const dispatch = useDispatch();
+	const user = JSON.parse(localStorage.getItem("profile"));
 
-    useEffect(() => {
-        dispatch(getMyBooks(user.user));
-    }, [dispatch]);
+	useEffect(() => {
+		dispatch(getMyBooks(user.user));
+	}, [dispatch]);
 
-    const {books} = useSelector((state) => state.books);
+	const { books } = useSelector((state) => state.books);
 
-    return(
-    <>
-      <Container>
-        <Typography variant="h2" align="center" marginTop='30px'>
-          My Bookshelf
-        </Typography>
-      </Container>
-      <div>
-        <Grid container spacing={2} justifyContent="center">
-          <Grid item>
-            <SearchGenre />
-          </Grid>
-        </Grid>
-      </div>
-      <Container sx={{ py: 8 }} maxWidth="lg">
-        <Grid container spacing={4}>
-        <Grid item>
-            <UploadBookButton />
-        </Grid> 
-          {books.map((book) => (
-            <Book key={book.BookId} book={book} />
-          ))}
-        </Grid>
-      </Container>
-    </>
-    )
-};
+	return (
+		<>
+			<Container>
+				<div className='header'>
+					<h1 className='header-text'>My Bookshelf</h1>
+				</div>
+			</Container>
+			<div>
+				<Grid container spacing={2} justifyContent='center'>
+					<Grid item>
+						<SearchGenre />
+					</Grid>
+				</Grid>
+			</div>
+			<Container sx={{ py: 8 }} maxWidth='lg'>
+				<Grid container spacing={4}>
+					<Grid item>
+						<UploadBookButton />
+					</Grid>
+					{books.map((book) => (
+						<Book key={book.BookId} book={book} />
+					))}
+				</Grid>
+			</Container>
+		</>
+	);
+}
