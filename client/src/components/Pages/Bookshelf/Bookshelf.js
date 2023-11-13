096a024aad866f6706d7bb8597181f7a44545118
YingrongChen <yingrong.chen@emory.edu>
2023-11-13 14:08:10 -0500

    debug books.map

--- client/src/components/Pages/Bookshelf/Bookshelf.js
+++ client/src/components/Pages/Bookshelf/Bookshelf.js
@@ -8,35 +8,35 @@ import { useSelector, useDispatch } from "react-redux";
 import { getBooks } from "../../../actions/books";
 
 export default function BookShelf() {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(getBooks());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
 
-	const { books } = useSelector((state) => state.books);
+  const { books } = useSelector((state) => state.books);
 
-	return (
-		<>
-			<Container>
-				<div className='header'>
-					<h1 className='header-text'>Bookshelf</h1>
-				</div>
-			</Container>
-			<div>
-				<Grid container spacing={2} justifyContent='center'>
-					<Grid item>
-						<SearchGenre />
-					</Grid>
-				</Grid>
-			</div>
-			<Container sx={{ py: 4 }} maxWidth='lg'>
-				<Grid container spacing={4}>
-					{books.map((book) => (
-						<Book key={book.BookId} book={book} />
-					))}
-				</Grid>
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Container>
+        <div className="header">
+          <h1 className="header-text">Bookshelf</h1>
+        </div>
+      </Container>
+      <div>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item>
+            <SearchGenre />
+          </Grid>
+        </Grid>
+      </div>
+      <Container sx={{ py: 4 }} maxWidth="lg">
+        <Grid container spacing={4}>
+          {Array.isArray(books)
+            ? books.map((book) => <Book key={book.BookId} book={book} />)
+            : []}
+        </Grid>
+      </Container>
+    </>
+  );
 }
--- client/src/components/Pages/MyBooks/MyBooks.js
+++ client/src/components/Pages/MyBooks/MyBooks.js
@@ -8,38 +8,38 @@ import { getMyBooks } from "../../../actions/books";
 import UploadBookButton from "./UploadBookButton";
 
 export default function MyBooks() {
-	const dispatch = useDispatch();
-	const user = JSON.parse(localStorage.getItem("profile"));
+  const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
 
-	useEffect(() => {
-		dispatch(getMyBooks(user.user));
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(getMyBooks(user.user));
+  }, [dispatch]);
 
-	const { books } = useSelector((state) => state.books);
-	return (
-		<>
-			<Container>
-				<div className='header'>
-					<h1 className='header-text'>My Bookshelf</h1>
-				</div>
-			</Container>
-			<div>
-				<Grid container spacing={2} justifyContent='center'>
-					<Grid item>
-						<SearchGenre />
-					</Grid>
-				</Grid>
-			</div>
-			<Container sx={{ py: 4 }} maxWidth='lg'>
-				<Grid container spacing={4}>
-					<Grid item>
-						<UploadBookButton />
-					</Grid>
-					{books.map((book) => (
-						<Book key={book.BookId} book={book} />
-					))}
-				</Grid>
-			</Container>
-		</>
-	);
+  const { books } = useSelector((state) => state.books);
+  return (
+    <>
+      <Container>
+        <div className="header">
+          <h1 className="header-text">My Bookshelf</h1>
+        </div>
+      </Container>
+      <div>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item>
+            <SearchGenre />
+          </Grid>
+        </Grid>
+      </div>
+      <Container sx={{ py: 4 }} maxWidth="lg">
+        <Grid container spacing={4}>
+          <Grid item>
+            <UploadBookButton />
+          </Grid>
+          {Array.isArray(books)
+            ? books.map((book) => <Book key={book.BookId} book={book} />)
+            : []}
+        </Grid>
+      </Container>
+    </>
+  );
 }
