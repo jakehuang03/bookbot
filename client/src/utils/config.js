eb9b88014d493dbc2f83b30f74e9a3727e0499b0
Lupin Cai <lupin.cai@emory.edu>
2023-11-16 17:12:37 -0500

    feature: testing

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -3,8 +3,8 @@ const config = {
 		baseURL: process.env.REACT_APP_API_URL,
 	},
 	development: {
-		// baseURL: "http://localhost:8000",
-		baseURL: "http://3.19.244.129",
+		baseURL: "http://localhost:8000/api",
+		//baseURL: "http://3.19.244.129",
 	},
 };
 
