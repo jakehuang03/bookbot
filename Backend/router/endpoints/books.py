b11960a0ef16af09bbd9b618f0d9df010f8e7535
jakehuang03 <zijianhuang03@gmail.com>
2023-11-29 22:59:08 -0500

    update getbook and post for books

--- Backend/router/endpoints/books.py
+++ Backend/router/endpoints/books.py
@@ -22,21 +22,20 @@ async def upload_file(
     try:
         userid = current_user["UserId"]
         if not userid:
-            raise HTTPException(status_code=400, detail="User auth faied")
+            raise HTTPException(status_code=400, detail="User auth failed")
+        
+        id = db.crud.create_book(
+                name=title, author=author, summary=summary, userid=userid, genre=genre
+        )
         upload_folder = Path("./uploaded_files")
         upload_folder.mkdir(exist_ok=True)
 
-        title = title + ".pdf"
-        
-        with (upload_folder / title).open("wb") as buffer:
+        upload_title = str(id) + ".pdf"
+        print(upload_title)
+        with (upload_folder / upload_title).open("wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
         
-        if os.path.exists(upload_folder / title):
-            print("file path exist")
-            id = db.crud.create_book(
-                name=title, author=author, summary=summary, userid=userid, genre=genre
-            )
-            return {"msg": "book uploaded", "bookid": id}
+        return {"msg": "book uploaded", "bookid": id}
         
     except Exception as e:
         raise HTTPException(detail=f"An error occurred: {e}", status_code=400)
@@ -54,16 +53,17 @@ async def searchBar(searchBook: str, genre: str):
     
 
 @router.get("/{bookId}")
-async def getBook(bookId: int,current_user: Annotated[dict, Depends(get_current_user)]):
+async def getBook(bookId: int, userId: int = None):
     try:
-        userid = current_user["UserId"]
-        if not userid:
-            raise HTTPException(status_code=400, detail="User auth faied")
-        print(userid)
         book = db.crud.get_book_by_id(bookId)
-        print(book)
         if not book:
             raise HTTPException(status_code=404, detail="Book not found")
+        if (not userId):
+            if(book.Published == False):
+                raise HTTPException(status_code=400, detail="User auth failed")
+        elif (book.UserId != userId) & (book.Published == False): 
+            raise HTTPException(status_code=400, detail="User auth failed")
+
         return book
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"An error occurred: {e}")
